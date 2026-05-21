619dab8231b545795eefff40458df874ecc217b5
Vittorio <liogplobo@gmail.com>
2026-05-21 17:31:05 +0100

    Added basic JSX code to Track.jsx, including the onButtonClick function prop and button label.

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -3,7 +3,10 @@ import styles from './Track.module.css';
 function Track( { track, buttonLabel, onButtonClick }) {
     return (
         <div className={styles.trackWrapper}>
-           
+           <h3>{track.name}</h3>
+           <p>{track.artist}</p>
+           <p>{track.album}</p>
+           <button onClick={() => onButtonClick(track)}>{buttonLabel}</button>
         </div>
     )
 }
