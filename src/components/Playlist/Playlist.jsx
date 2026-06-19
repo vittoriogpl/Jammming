d2969c28ad0c400bf92dc1b7f505607858af8037
Vittorio <liogplobo@gmail.com>
2026-06-19 17:09:38 +0100

    Replaced the <h2> in Playlist with a controlled input to let the user name the playlist.

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -4,7 +4,13 @@ import Tracklist from '../Tracklist/Tracklist';
 function Playlist({ tracks, buttonLabel, onButtonClick, playlistName, onNameChange, onSavePlaylist }) {
     return (
         <div className={styles.playlistWrapper}>
-            <h2>My Playlist</h2>
+            <input
+                className={styles.playlistNameInput}
+                type="text"
+                placeholder="Enter playlist name"
+                value={playlistName}
+                onChange={onNameChange}
+            />
             <Tracklist tracks={tracks} buttonLabel={buttonLabel} onButtonClick={onButtonClick}/>
             <button className={styles.savePlaylistBtn} onClick={onSavePlaylist}>Save to Spotify</button>
         </div>
